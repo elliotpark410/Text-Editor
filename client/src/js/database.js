fca6eb44327e110dd9024b822f3c6755c2146024
Elliot Park <elliotpark410@gmail.com>
2022-03-02 02:18:45 -0800

    fix bug in src-sw.js and require StaleWhileRevalidate

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.error('putDb not implemented');
+  // console.error('putDb not implemented');
 
   console.log('PUT request to update content: ', content);
 
@@ -38,7 +38,7 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  console.error('getDb not implemented');
+  // console.error('getDb not implemented');
 
   console.log('GET request to get all content');
 
