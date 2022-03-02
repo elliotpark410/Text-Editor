f4b0d05899a90217d8ad0fa025980a3ec5b7b774
Elliot Park <elliotpark410@gmail.com>
2022-03-01 23:59:19 -0800

    in src/js/database.js, add logic for putDB method that accepts some content and adds it to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+
+  console.log('PUT from the database to update content', content);
+
+  // Create a connection to the database database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .delete() method to get all data in the database.
+  const request = store.put(content);
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
